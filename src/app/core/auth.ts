a36dd4add9b391ffcb8f6121956570fabd9d9926
Mgreat01 <ephraimmonga5@gmail.com>
2026-06-15 15:14:35 +0200

    feat  : creation et implementation du service gerant le profil user

---
+++ src/app/core/auth.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private url = 'http://localhost:8000/api/auth/';
+
+  constructor(private http: HttpClient) {}
+
+  login(user: User): Promise<any> {
+    const credentials = {
+      email: user.email,
+      password: user.password,
+    };
+    return firstValueFrom(this.http.post(this.url + 'login/', credentials));
+  }
+
+  register(user: User): Promise<any> {
+    const payload = {
+      ...user,
+      role: 'user', 
+    };
+    return firstValueFrom(this.http.post(this.url + 'register/', payload));
+  }
+}
