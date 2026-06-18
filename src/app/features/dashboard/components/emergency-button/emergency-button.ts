a4a84de825382d9f1d26bbf66013d0758e56f33d
Mgreat01 <ephraimmonga5@gmail.com>
2026-06-18 18:45:25 +0200

    feat : creation et implemantation de la page emergency-button

---
+++ src/app/features/dashboard/components/emergency-button/emergency-button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emergency-button',
+  imports: [],
+  templateUrl: './emergency-button.html',
+  styleUrl: './emergency-button.css',
+})
+export class EmergencyButton {}
