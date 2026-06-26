b77dab185c8f94831f99bf6cef65994f2a028fad
Mgreat01 <ephraimmonga5@gmail.com>
2026-06-26 18:36:34 +0200

    refactor : refonte du rendu du register

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';   
-import { User } from '../../../models/user';
-import { Auth } from '../../../core/auth';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../../core/auth';
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-register',
-  standalone: true,             
-  imports: [FormsModule,CommonModule],        
+  standalone: true,
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './register.html',
   styleUrls: ['./register.css']
 })
@@ -19,25 +19,22 @@ export class Register {
     password: ''
   };
 
-  message: string = '';
-  error: string = '';
+  message = '';
+  error = '';
 
-  constructor(private auth: Auth , private router: Router) {}
+  constructor(private auth: Auth, private router: Router) {}
 
   onSubmit(): void {
     this.auth.register(this.user)
-      .then(response => {
-        let d = response.access_token;
-        //let d = 's'
-        this.message = `Inscription réussie ! Redirection... ${d}`;
-        console.log(response);
+      .then(() => {
+        this.message = 'Inscription réussie ! Redirection...';
+        this.error = '';
         this.router.navigate(['/login']);
       })
       .catch(err => {
         this.error = err.error?.detail || 'Erreur lors de l\'inscription';
         this.message = '';
         console.error(err.error?.detail);
-        
       });
   }
 }
